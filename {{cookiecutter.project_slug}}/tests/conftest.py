b0c90e11f116a024249230bbc7d824b31383ab16
Li Lin <guru.lin@gmail.com>
2024-06-21 13:39:53 +0800

    adjusted connect_args based on database type

--- {{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -36,7 +36,9 @@ def tmp_sqlite_url():
 
 
 test_db_url = os.environ.get("TEST_DATABASE_URI", tmp_sqlite_url())
-testing_sql_engine = create_engine(test_db_url, connect_args={"check_same_thread": False}, echo=False)
+if test_db_url.startswith("sqlite"):
+    db_args = {"check_same_thread": False}
+testing_sql_engine = create_engine(test_db_url, connect_args=db_args, echo=False)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=testing_sql_engine)
 
 
