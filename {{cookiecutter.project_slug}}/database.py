f019d1559fa56f4d71045de3b59073a3dfcd99fc
Li Lin <guru.lin@gmail.com>
2024-06-23 20:23:31 +0800

    polish

--- {{cookiecutter.project_slug}}/database.py
+++ {{cookiecutter.project_slug}}/database.py
@@ -16,11 +16,13 @@ db_url = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///memory:")
 #
 conn_args = {"check_same_thread": False} if db_url.startswith("sqlite") else {}
 
+db_url = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///memory:")
 engine = create_engine(db_url, connect_args=conn_args, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 #
 # begin of async db setup, uncomment and remove sync setup above if needed
 #
+# db_url = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite+aiosqlite:///memory:")
 # engine = create_async_engine(db_url, echo=False)
 # SessionLocal = async_sessionmaker(bind=engine, autoflush=False, future=True)
