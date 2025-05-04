594cf983f399a784e0629e81a2f16c619c114308
Li Lin <guru.lin@gmail.com>
2025-05-04 15:02:20 +0800

    remove invalid rule

--- {{cookiecutter.project_slug}}/{{ cookiecutter.pkg_name }}/models.py
+++ {{cookiecutter.project_slug}}/{{ cookiecutter.pkg_name }}/models.py
@@ -14,5 +14,5 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)  # noqa: A003, VNE003
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)  # noqa: A003
     login_name: Mapped[str] = mapped_column(String(32))
